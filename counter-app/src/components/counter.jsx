c5a3300667bfa1439e9efbbd1c62997528487834
Akshat-Rana <akshat511716@gmail.com>
2021-06-20 11:43:13 +0530

    Created a basic task tracker with reminder

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,27 +1,48 @@
 import React, { Component } from "react";
-
 class Counter extends Component {
-  state = {
-    count: 0,
+  state = { count: 0 };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
+
   render() {
+    const myStyle = {
+      backgroundColor: "red",
+      fontSize: "5rem",
+      // margin: " 5rem auto",
+      fontWeight: "Bold",
+      // height: "100vh",
+      display: "grid",
+      justifySelf: "center",
+      gridTemplateColumns: "1fr 1fr",
+      // gridGap: "10rem",
+    };
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <section style={myStyle}>
+          <span className={this.getBagdeClasses()}>{this.formatCount()}</span>
+          <button
+            onClick={this.handleIncrement}
+            className="btn btn-secondary btn-xl"
+          >
+            Increment
+          </button>
+        </section>
       </div>
     );
   }
-  getBadgeClasses() {
+
+  getBagdeClasses = () => {
     let classes = "badge m-2 bg-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
-  }
+  };
 
-  formatCounter() {
-    const { count } = this.state;
+  formatCount = () => {
+    const count = this.state.count;
     return count === 0 ? "Zero" : count;
-  }
+  };
 }
 
 export default Counter;
