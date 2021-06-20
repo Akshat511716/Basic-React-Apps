30fd66f5ee3950d3a173e245b905dcd8cff9c23f
Akshat-Rana <akshat511716@gmail.com>
2021-06-20 12:12:54 +0530

    Added task adding feature

--- task-tracker/src/components/AddTast.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AddTast = () => {
-  return <div></div>;
-};
-
-export default AddTast;
--- task-tracker/src/components/Button.js
+++ task-tracker/src/components/Button.js
@@ -1,17 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Button = ({ color, text }) => {
-  const onClick = () => {
-    console.log("click");
-  };
-
+const Button = ({ color, text, onAdd }) => {
   return (
-    <button
-      onClick={onClick}
-      style={{ backgroundColor: color }}
-      className="btn"
-    >
+    <button onClick={onAdd} style={{ backgroundColor: color }} className="btn">
       {text}
     </button>
   );
