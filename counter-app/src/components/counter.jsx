175bfcf26db22ed756d10e90dc240f8f1f5b1333
Akshat-Rana <akshat511716@gmail.com>
2021-07-03 12:53:38 +0530

    Updated the files

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,23 +1,17 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = { value: this.props.value };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBagdeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -26,16 +20,16 @@ class Counter extends Component {
     );
   }
 
-  getBagdeClasses = () => {
+  getBagdeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
-  };
+  }
 
-  formatCount = () => {
-    const count = this.state.value;
-    return count === 0 ? "Zero" : count;
-  };
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
 }
 
 export default Counter;
